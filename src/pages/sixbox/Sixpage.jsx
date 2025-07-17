b108d3872ad2cd2c4040d5b281644234424d6020
shamsiddin <sirojiddins881@gmail.com>
2025-07-17 20:21:36 +0300

    Yana rasmdagi xatolik tuzatildi

--- src/pages/sixbox/Sixpage.jsx
+++ src/pages/sixbox/Sixpage.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 export default function Sixpage() {
   return (
     <div className=' max-w-[1200px] m-auto h-[400px] rounded-[18px] overflow-hidden mb-[24px]'>
-      <NavLink to={'/air'}><img className=' w-full h-full object-cover transition-all duration-200 hover:scale-105' src='../public/images/bgimgair.jpg' alt="" /></NavLink>
+      <NavLink to={'/air'}><img className=' w-full h-full object-cover transition-all duration-200 hover:scale-105' src='/images/bgimgair.jpg' alt="" /></NavLink>
       
     </div>
   )
