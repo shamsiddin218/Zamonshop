3e539a810da13e62244949e916e7038a52a87204
shamsiddin <sirojiddins881@gmail.com>
2025-07-17 20:31:53 +0300

    Xato tuzatildi

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,30 +5,35 @@ import Footer from './Footer';
 import HomeSkeleton from '../skleton/HomeSkeleton';
 
 export default function Layout() {
-const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
-useEffect(() => {
-  fetch("/Language/uz.json") // yoki local data bo‘lsa
-    .then(res => res.json())
-    .then(data => {
-      // data bilan ishlang
+  useEffect(() => {
+    const handleLoad = () => {
       setLoading(false);
-    });
-}, []);
+    };
 
+    if (document.readyState === 'complete') {
+      // Sayt allaqachon yuklangan bo'lsa
+      setLoading(false);
+    } else {
+      // Yuklanishini kutamiz
+      window.addEventListener('load', handleLoad);
+    }
+
+    return () => window.removeEventListener('load', handleLoad);
+  }, []);
 
   return (
     <>
-    {loading ? (
-      <HomeSkeleton/>
-    ) : (
-
-      <>
-      <Navbar />
-      <Outlet />
-      <Footer />
-      </>
-    )}
+      {loading ? (
+        <HomeSkeleton />
+      ) : (
+        <>
+          <Navbar />
+          <Outlet />
+          <Footer />
+        </>
+      )}
     </>
   );
 }
